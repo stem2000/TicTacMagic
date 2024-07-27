099a202f80db5d3107da2b682907c9968c98675d
stem <zaregenilsya@yandex.by>
2024-07-27 22:21:23 +0300

    feature: GoToMarker deleted from PLayerView

--- Assets/_Project/Entities/Player/PlayerView.cs
+++ Assets/_Project/Entities/Player/PlayerView.cs
@@ -7,21 +7,9 @@ namespace TicTacMagic
     {
         private IPlayer player;
 
-        [SerializeField] private GoToMarker goToMarker;
-
         public void Initialize(IPlayer player)
         {
             this.player = player;
-            InitializeMarker();
-        }
-
-        private void InitializeMarker()
-        {
-            if (goToMarker != null)
-            {
-                goToMarker = Instantiate(goToMarker);
-                goToMarker.Initialize(player);
-            }
         }
 
     }

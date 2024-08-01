bd7a8982efe597200704dbed00014ca0518d3480
stem <zaregenilsya@yandex.by>
2024-08-01 13:59:13 +0300

    feature: added fire

---
+++ Assets/_Project/Entities/GameObjects/Fire/Fire.cs
@@ -0,0 +1,43 @@
+using MEC;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TicTacMagic
+{
+    public class Fire : TileObject
+    {
+        [SerializeField] float damage = 10;
+        [SerializeField] float damageCooldown = 1f;
+        [SerializeField] CircleCollider2D myCollider;
+
+        public override bool IsMoveBlocker()
+        {
+            return false;
+        }
+
+        public override IEnumerator<float> StartDestroing(float lifetime)
+        {
+            yield return Timing.WaitForSeconds(lifetime);
+            Destroy(gameObject);
+        }
+
+        public void OnTriggerEnter2D(Collider2D collision)
+        {
+            IDamageable damageable;
+
+            if (collision.TryGetComponent(out damageable))
+            {
+                damageable.GetDamage(damage);
+                Timing.RunCoroutine(StartCooldown().CancelWith(gameObject));
+            }
+        }
+
+        private IEnumerator<float> StartCooldown()
+        {
+            myCollider.enabled = false;
+            yield return Timing.WaitForSeconds(damageCooldown);
+            myCollider.enabled = true;
+        }
+    }
+}

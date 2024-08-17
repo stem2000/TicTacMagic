9d08f42c3d96ac66c02076792b5d713385028478
stem <zaregenilsya@yandex.by>
2024-08-17 10:52:39 +0300

    prefab stats changes

--- Assets/_Project/Scripts/BoundsPrompter/BoundsPromter.cs
+++ Assets/_Project/Scripts/BoundsPrompter/BoundsPromter.cs
@@ -1,9 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using Unity.VisualScripting;
 using UnityEngine;
-using static UnityEditor.ShaderGraph.Internal.KeywordDependentCollection;
 
 namespace TicTacMagic
 {

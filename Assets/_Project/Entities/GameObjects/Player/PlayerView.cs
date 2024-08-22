f40d508929408dd9f9efca066fa4c5c43125749a
stem <zaregenilsya@yandex.by>
2024-08-22 14:43:33 +0300

    feature: player damage effect

--- Assets/_Project/Entities/GameObjects/Player/PlayerView.cs
+++ Assets/_Project/Entities/GameObjects/Player/PlayerView.cs
@@ -1,3 +1,5 @@
+using MEC;
+using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -5,14 +7,29 @@ namespace TicTacMagic
 {
     public class PlayerView : MonoBehaviour
     {
-        private IPlayer player;
+        [SerializeField] private SpriteRenderer spriteRenderer;
+        [SerializeField] private float damageHighlightDuration = 0.3f;
+        [SerializeField] private Color damageHiglihghtColor = Color.red;
+        private IPlayer player;        
 
 
         public void Initialize(IPlayer player)
         {
             this.player = player;
+            player.AddListenerToPlayerDamaged(PlayDamageEffect);
         }
 
+        private void PlayDamageEffect(float damage)
+        {
+            Timing.RunCoroutine(_DamageEffectRoutine().CancelWith(gameObject));
+        }
+
+        IEnumerator<float> _DamageEffectRoutine()
+        {
+            spriteRenderer.color = damageHiglihghtColor;
+            yield return Timing.WaitForSeconds(damageHighlightDuration);
+            spriteRenderer.color = Color.white;
+        }
 
     }
 }
